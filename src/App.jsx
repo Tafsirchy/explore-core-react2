94f1ab30a109192248230ae73ec3047b703bc97d
Tafsirchy <tafsirchy1000@gmail.com>
2025-10-19 20:34:26 +0600

    Different ways to handle events in React

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+
+  function handleClick() {
+    alert("I am Clicked");
+  }
+  const handleClick2 = () => {
+    alert("I am Clicked 2");
+  }
+
+  const add5 = (num) => {
+    const newNum = num + 5;
+    alert(`Your Number is ${newNum}`);
+  }
+
+  return (
+    <> 
+      <h1>Vite + React</h1>
+      {/* <button onClick="handleClick()">Click Me</button> */}
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click Me</button>
+      {/* <button onClick={() => alert("I am Clicked 3")}>Click Me</button>
+      <button onClick={function handleClick4(){ alert("I am Clicked 4")}}>Click Me</button> */}
+
+      {/* wrap by using arrow function */}
+      <button onClick={() => add5(13)}>Add 5</button>
+    </>
+  )
+}
+
+export default App
